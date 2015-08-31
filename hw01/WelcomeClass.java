d7a1b8be817e6a16342509d80b3da2879fac5360
gcc219 <gcc219@lehigh.edu>
2015-08-31 14:09:41 +0000

    First Homework Assignment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//George Cooper
+//HW 01
+//Welcome Class Java Program
+//
+//  first compile the program using the terminal command:
+//      javac WelcomeClass.java
+//  Then, run the program using the terminal command
+//      java WelcomeClass
+
+//  Class Definition
+public class WelcomeClass{
+
+//  Main Method
+    public static void main(String[] args){
+        
+//  printing the welcome message
+    System.out.println("  -----------");
+    System.out.println("  | Welcome |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-G--C--C--2--1--9-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("Over the summer, I spent 3 weeks relaxing with my family in Costa Rica.");
+    }
+}
